fc2d01d23485bc8e303a17188647e82dba090f54
jssilvasv3 <jsilvasv3@gmail.com>
2025-03-27 01:16:57 -0300

    Update tor_termux.sh

--- TorTermux/tor_termux.sh
+++ TorTermux/tor_termux.sh
@@ -16,6 +16,19 @@ ControlPort 9051
 CookieAuthentication 1
 EOF
 
-echo "[*] Configuração concluída! Para iniciar o Tor, execute:"
-echo "tor -f $HOME/.tor/torrc"
+echo "[*] Instalação concluída!"
+echo "[*] Digite 'tor' para iniciar."
+
+# Esperar 2 segundos antes de iniciar o Tor automaticamente
+sleep 2
+
+echo "[*] Iniciando o Tor..."
+tor -f $HOME/.tor/torrc | while read line; do
+    echo "$line"
+    if echo "$line" | grep -q "Bootstrapped 100%"; then
+        echo "[*] Conexão Pronta!"
+        echo "[*] Para encerrar, pressione Ctrl + C"
+    fi
+done
+
 
